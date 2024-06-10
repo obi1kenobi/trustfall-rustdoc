45a410128045a422cc842997f15a15f352ef65b2
Predrag Gruevski <obi1kenobi82@gmail.com>
2024-06-10 14:17:53 +0000

    Add support for rustdoc JSON format v30.

--- src/parser.rs
+++ src/parser.rs
@@ -64,6 +64,13 @@ pub fn load_rustdoc(path: &Path) -> anyhow::Result<VersionedCrate> {
             format_version,
         )?)),
 
+        #[cfg(feature = "v30")]
+        30 => Ok(VersionedCrate::V30(parse_or_report_error(
+            path,
+            &file_data,
+            format_version,
+        )?)),
+
         _ => bail!(
             "rustdoc format v{format_version} for file {} is not supported",
             path.display()
