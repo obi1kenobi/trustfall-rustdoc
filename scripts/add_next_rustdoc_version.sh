f4612af84a1ba0bd2816a6374a9163294797888e
Predrag Gruevski <obi1kenobi82@gmail.com>
2024-06-14 04:51:12 +0000

    Add code-generation facilities for adding new rustdoc versions.

---
+++ scripts/add_next_rustdoc_version.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Script requirements:
+# - python
+# - yq
+
+# Fail on first error, on undefined variables, and on failures in pipelines.
+set -euo pipefail
+
+# Go to the repo root directory.
+cd "$(git rev-parse --show-toplevel)"
+
+CURRENT_VERSIONS="$(yq '.features.default' Cargo.toml -o json | \
+    python -m json.tool --compact | \
+    sed 's/,/, /g' | \
+    sed 's/\[//g' | \
+    sed 's/]//g')"
+NEXT_VERSION_NUMBER="$(yq '.features.default.[-1] | sub("v(\d+)", "${1}") | to_number | (. + 1)' Cargo.toml -o json -r)"
+
+ALL_VERSIONS="$(yq '.features.default[] | sub("v(\d+)", "${1}")' Cargo.toml -o json -r) ${NEXT_VERSION_NUMBER}"
+
+# Generate the new Rust source for the specified versions.
+pushd crates/generator
+cargo run -- $ALL_VERSIONS
+popd
+
+# Reformat the generated Rust source code.
+cargo fmt
+
+# Update the Cargo.toml file to add the new dependency and feature number.
+
+# '1h;2,$H;$!d;g' means "look two lines at a time":
+# https://unix.stackexchange.com/questions/26284/how-can-i-use-sed-to-replace-a-multi-line-string
+sed -e '1h;2,$H;$!d;g' \
+    -e "s/\n\[features\]/trustfall-rustdoc-adapter-v${NEXT_VERSION_NUMBER} = { package = \"trustfall-rustdoc-adapter\", version = \">=${NEXT_VERSION_NUMBER}.0.0,<${NEXT_VERSION_NUMBER}.1.0\", optional = true }\n\n[features]/" \
+    -i Cargo.toml
+
+DEFAULT_MATCHER="default = \[${CURRENT_VERSIONS}\]"
+sed -e "s/$DEFAULT_MATCHER/default = [${CURRENT_VERSIONS}, \"v${NEXT_VERSION_NUMBER}\"]/" \
+    -i Cargo.toml
+
+echo "v${NEXT_VERSION_NUMBER} = [\"dep:trustfall-rustdoc-adapter-v${NEXT_VERSION_NUMBER}\"]" >>Cargo.toml
+
+# Ensure cargo regenerates the lockfile.
+cargo check
