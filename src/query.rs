26a8e3590f9e1e88234b7182e9dad0e13b0031da
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2024-08-18 17:42:35 -0400

    Release v0.16 with sealed trait support and without rustdoc v27. (#47)

--- src/query.rs
+++ src/query.rs
@@ -13,11 +13,6 @@ impl<'a> VersionedRustdocAdapter<'a> {
         vars: BTreeMap<K, V>,
     ) -> anyhow::Result<Box<dyn Iterator<Item = QueryResult> + 'a>> {
         match self {
-            #[cfg(feature = "v27")]
-            VersionedRustdocAdapter::V27(_, adapter) => {
-                execute_query(self.schema(), adapter.clone(), query, vars)
-            }
-
             #[cfg(feature = "v28")]
             VersionedRustdocAdapter::V28(_, adapter) => {
                 execute_query(self.schema(), adapter.clone(), query, vars)
